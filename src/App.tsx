97d8cd8aca4be5e9119da2d2ba1d17e3aee0bf8b
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-28 12:36:03 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center font-bold text-3xl'>
+        Hello vite World
+      </div>
+    </>
+  )
+}
+
+export default App
