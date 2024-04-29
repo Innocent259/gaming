0c2683ccd75522e198542c4d3180c381ef7aa46e
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-29 10:14:23 +0200

    Render components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './index.css'
+import Profile from './components/Profile'
+import Browse from './components/Browse'
+import backgroundImage from './assets/images/screen.png'
 
 function App() {
 
   return (
-    <>
-      <div className='text-center font-bold text-3xl'>
-        Hello vite World
-      </div>
-    </>
+    <div className='h-screen flex flex-col justify-between' style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <Profile />
+      <Browse />
+    </div>
   )
 }
 
