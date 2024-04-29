f8ad1677e0840382e1f703086d015caee72599a2
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-29 10:14:45 +0200

    Create dummy data

---
+++ src/assets/data.ts
@@ -0,0 +1,53 @@
+export const data = [
+  {
+    id: 1,
+    profile: [
+      {
+        name: "Maxwell",
+        image:
+          "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600",
+        highlights: [
+          {
+            totalMatche: 200,
+            competitive: 390,
+            totalWins: 73,
+            achievements: 24,
+          },
+        ],
+      },
+    ],
+    ranking: [
+      {
+        url: "https://images.pexels.com/photos/139392/checkmate-chess-resignation-conflict-139392.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title: "Chess",
+        score: "1",
+      },
+      {
+        url: "https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Warships",
+        score: "5",
+      },
+      {
+        url: "https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "Warships",
+        score: "5",
+      },
+    ],
+    earning: "$9,885",
+    pearls: ["Collect pearls to ...", "Make sure you correct..."],
+    yourKokomo: [
+      {
+        title: "High Roler",
+        url: "https://images.pexels.com/photos/14358446/pexels-photo-14358446.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        title: "High Roler",
+        url: "https://images.pexels.com/photos/14358446/pexels-photo-14358446.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        title: "High Roler",
+        url: "https://images.pexels.com/photos/14358446/pexels-photo-14358446.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+    ],
+  },
+];
