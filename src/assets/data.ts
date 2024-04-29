07f9188db4c777ca6be612f13073eb44d92a37e9
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-29 18:31:25 +0200

    Update data file

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -3,12 +3,12 @@ export const data = [
     id: 1,
     profile: [
       {
-        name: "Maxwell",
+        username: "chessdolla22.eth",
         image:
           "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600",
         highlights: [
           {
-            totalMatche: 200,
+            totalMatches: 200,
             competitive: 390,
             totalWins: 73,
             achievements: 24,
@@ -29,8 +29,8 @@ export const data = [
       },
       {
         url: "https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Warships",
-        score: "5",
+        title: "Crowd Favorite",
+        score: "#2",
       },
     ],
     earning: "$9,885",
