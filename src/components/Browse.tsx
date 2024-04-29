c27f3b4b43012683227678470712bd24054bab9e
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-29 10:15:05 +0200

    Develop Browse component

---
+++ src/components/Browse.tsx
@@ -0,0 +1,11 @@
+import other from '../assets/images/Other games.png'
+
+const Browse: React.FC = () => {
+  return (
+    <div className='mt-4'>
+      <img src={other} alt="Other Games" />
+    </div>
+  )
+}
+
+export default Browse
