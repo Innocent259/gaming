d09699dc2f736130a865a2da8f9a56ee988cab3e
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-30 10:02:55 +0200

    Render other games

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -5,14 +5,28 @@ import otherButton from '../assets/images/other button.png'
 const Browse: React.FC = () => {
   return (
     <div className='z-50 flex justify-between bg-cyan-500 relative'>
-      <div className='bg-no-repeat bg-left w-8' style={{ backgroundImage: `url(${leftFlower})` }}>
+      <div className='bg-no-repeat bg-left w-1/6' style={{ backgroundImage: `url(${leftFlower})` }}>
       </div>
-      <div className='flex items-center justify-center w-10/12  h-36 bg-transparent'>
+      <div className='flex items-center justify-center h-36 bg-transparent'>
         <button className='absolute left-1/2 -top-6 transform -translate-x-1/2'>
           <img src={otherButton} alt="Button " />
         </button>
+        <ul className='flex flex-col gap-2'>
+          <li>
+            <a href="" className='flex items-center gap-2'>
+              <img src="https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Warships" className='w-10 h-10' />
+              <h2>Warships</h2>
+            </a>
+          </li>
+          <li>
+            <a href="" className='flex gap-2 items-center'>
+              <img src="https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Warships" className='w-10 h-10' />
+              <h2>Warships</h2>
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className='bg-no-repeat bg-right-bottom w-8' style={{ backgroundImage: `url(${rightFlower})` }}>
+      <div className='bg-no-repeat bg-right-bottom w-1/4' style={{ backgroundImage: `url(${rightFlower})` }}>
       </div>
     </div>
   );
