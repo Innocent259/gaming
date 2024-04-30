fb8cebdedd8553f9070e1d259d59773134e0080a
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-30 15:08:51 +0200

    Add border radius

--- src/components/ProfileElements.tsx
+++ src/components/ProfileElements.tsx
@@ -25,21 +25,21 @@ const ProfileElements = () => {
                                 data.map(item => (
                                     <div key={item.id} className='flex flex-col gap-2 uppercase'>
                                         <div className='flex gap-2 items-center'>
-                                            <img src={item.ranking[0].url} alt="Game 1" className='w-10 h-10' />
+                                            <img src={item.ranking[0].url} alt="Game 1" className='w-10 h-10 rounded-lg' />
                                             <div className='flex flex-col py-1'>
                                                 <h1 className='text-xs text-black font-extrabold text-shadow'>{item.ranking[0].title}</h1>
                                                 <p className='text-[8px]'>top {item.ranking[0].score}%</p>
                                             </div>
                                         </div>
                                         <div className='flex gap-2 justify-items-center'>
-                                            <img src={item.ranking[1].url} alt="Rank 2" className='w-10 h-10' />
+                                            <img src={item.ranking[1].url} alt="Rank 2" className='w-10 h-10 rounded-lg' />
                                             <div className='flex flex-col py-1'>
                                                 <h1 className='text-xs text-black font-extrabold text-shadow'>{item.ranking[1].title}</h1>
                                                 <p className='text-[8px]'>top {item.ranking[1].score}</p>
                                             </div>
                                         </div>
                                         <div className='flex gap-2 items-center'>
-                                            <img src={item.ranking[2].url} alt="Rank 3" className='w-10 h-10' />
+                                            <img src={item.ranking[2].url} alt="Rank 3" className='w-10 h-10 rounded-lg' />
                                             <div className='flex flex-col py-1'>
                                                 <h1 className='text-xs text-black font-extrabold text-shadow'>{item.ranking[2].title}</h1>
                                                 <p className='text-[8px]'>{item.ranking[2].score}</p>
