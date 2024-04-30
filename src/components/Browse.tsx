681862760e276a2dafe85396ad1ff14c957cbfc4
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-30 09:40:19 +0200

    Add browse button

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,11 +1,20 @@
-import other from '../assets/images/Other games.png'
+import leftFlower from '../assets/images/other flower-l.png';
+import rightFlower from '../assets/images/other flower-r.png'
+import otherButton from '../assets/images/other button.png'
 
 const Browse: React.FC = () => {
   return (
-    <div className='mt-4'>
-      <img src={other} alt="Other Games" />
+    <div className='z-50 flex justify-between bg-cyan-500 relative'>
+      <div className='bg-no-repeat bg-left w-8' style={{ backgroundImage: `url(${leftFlower})` }}>
+      </div>
+      <div className='flex items-center justify-center w-10/12  h-36 bg-transparent'>
+        <button className='absolute left-1/2 -top-6 transform -translate-x-1/2'>
+          <img src={otherButton} alt="Button " />
+        </button>
+      </div>
+      <div className='bg-no-repeat bg-right-bottom w-8' style={{ backgroundImage: `url(${rightFlower})` }}>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Browse
+export default Browse;
