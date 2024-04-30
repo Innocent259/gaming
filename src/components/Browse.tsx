1c13c2cbe0f44a570a73361066087f854d38867a
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-30 10:38:02 +0200

    Update Browse component

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -7,21 +7,27 @@ const Browse: React.FC = () => {
     <div className='z-50 flex justify-between bg-cyan-500 relative'>
       <div className='bg-no-repeat bg-left w-1/6' style={{ backgroundImage: `url(${leftFlower})` }}>
       </div>
-      <div className='flex items-center justify-center h-36 bg-transparent'>
+      <div className='flex items-center justify-center pb-2 bg-transparent'>
         <button className='absolute left-1/2 -top-6 transform -translate-x-1/2'>
-          <img src={otherButton} alt="Button " />
+          <img src={otherButton} alt="Button" />
         </button>
-        <ul className='flex flex-col gap-2'>
-          <li>
+        <ul className='flex flex-col gap-2 pt-8'>
+          <li className='px-4'>
             <a href="" className='flex items-center gap-2'>
-              <img src="https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Warships" className='w-10 h-10' />
-              <h2>Warships</h2>
+              <img src="https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Warships" className='w-10 h-10 rounded-lg' />
+              <div className='py-1'>
+                <h2 className='text-xs font-extrabold text-shadow-1'>WARSHIPS</h2>
+                <p className='uppercase text-[8px]'>In the ultimate PVP, sink the enemy navy to win the pot!</p>
+              </div>
             </a>
           </li>
-          <li>
-            <a href="" className='flex gap-2 items-center'>
-              <img src="https://images.pexels.com/photos/18959222/pexels-photo-18959222/free-photo-of-battleships-sailing-on-the-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Warships" className='w-10 h-10' />
-              <h2>Warships</h2>
+          <li className='px-4'>
+            <a href="" className='flex items-center gap-2'>
+              <img src="https://wallpapers.com/images/featured/cute-little-puppies-jggd1xqev9439k6k.jpg" alt="Warships" className='w-10 h-10 rounded-lg' />
+              <div className='py-1'>
+                <h2 className='text-xs font-extrabold text-shadow-1'>CROWD FAVORITE</h2>
+                <p className='text-[8px]'>FROM CUTE PUPPIES TO SPORTING GOATs, PREDICT THE CROWD'S FAVORITE AND WIN!</p>
+              </div>
             </a>
           </li>
         </ul>
